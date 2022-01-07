833a69b0e272cf37a3184aa0d059906586257337
zhangsen <maizhanwangle@163.com>
2022-01-07 15:08:38 +0800

    feat: add jstable

---
+++ src/components/js-table.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    tableData: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    tableHeader: {
+      type: Array,
+      required: true,
+    },
+  },
+  render: function (h) {
+    return (
+      <el-table data={this.tableData}>
+        {this.tableHeader.map(function (item) {
+          return <el-table-column prop={item.prop} label={item.label} />;
+        })}
+      </el-table>
+    );
+  },
+};
+</script>
---
+++ src/components/my-table.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-table :data="data">
+    <slot></slot>
+  </el-table>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  created() {
+      console.log(Array.isArray(this.$slots));
+  }
+};
+</script>
