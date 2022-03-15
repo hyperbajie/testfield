3e2c2687351fad744bfa56d0659ff6ef2e01ac6c
zhangsen <maizhanwangle@163.com>
2022-03-15 11:18:42 +0800

    feat: complete

--- src/views/test-field.vue
+++ src/views/test-field.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
     <h3>test-field</h3>
-    <el-button @click="show = !show">toggle</el-button>
+    <el-button @click="show = !show">toggle show</el-button>
     <transition name="fade">
       <p v-show="show">hide text</p>
     </transition>
+    <el-divider></el-divider>
+    <el-button @click="pageShow = !pageShow">toggle pageShow</el-button>
+    <el-dialog :visible.sync="pageShow">
+      <p>第一行</p>
+      <p>第二行</p>
+    </el-dialog>
   </div>
 </template>
 
@@ -13,6 +19,7 @@ export default {
   data() {
     return {
       show: false,
+      pageShow: false,
     };
   },
 };
