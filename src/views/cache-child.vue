b1c652db7c7116e61e422f8e38c01a99dc324df8
zhangsen <maizhanwangle@163.com>
2022-03-21 09:39:19 +0800

    feat: do extends

---
+++ src/views/cache-child.vue
@@ -0,0 +1,18 @@
+<script>
+import CacheParent from "./cache-parent.vue";
+export default {
+  name: "CacheChild",
+    extends: CacheParent,
+//   mixins: [CacheParent],
+  data() {
+    return {
+      title: "cache child",
+    };
+  },
+  methods: {
+    handleClickOne() {
+      console.warn("im in cachechild page");
+    },
+  },
+};
+</script>
