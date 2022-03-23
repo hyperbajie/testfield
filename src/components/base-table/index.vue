7f0e78371fe16be7b547275ccfbc2fd1389304bf
zhangsen <maizhanwangle@163.com>
2022-03-23 10:24:47 +0800

    feat: customize table base on el-table

---
+++ src/components/base-table/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <el-button @click="handleClickCustomize">自定义</el-button>
+    <el-table v-bind="$attrs">
+      <slot></slot>
+    </el-table>
+    <el-dialog :visible.sync="show">
+      <template v-slot:title> 自定义 </template>
+      <el-checkbox
+        v-for="(item, index) in tempOptions"
+        :key="index"
+        v-model="item.show"
+        >{{ item.label }}</el-checkbox
+      >
+      <template v-slot:footer>
+        <el-button @click="handleClickConfirm">确认</el-button>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      options: [],
+      tempOptions: [],
+      oldDefaultSlots: null,
+      oriDefaultSlotsObj: {},
+    };
+  },
+  mounted() {
+    this.oldDefaultSlots = this.$slots.default;
+    this.$slots.default.forEach((item) => {
+      this.options.push({
+        ...item.componentOptions.propsData,
+        show: true,
+      });
+      this.oriDefaultSlotsObj[
+        item.componentOptions.propsData.prop ||
+          item.componentOptions.propsData.type
+      ] = item;
+    });
+  },
+  methods: {
+    handleClickCustomize() {
+      this.tempOptions = JSON.parse(JSON.stringify(this.options));
+      this.show = true;
+    },
+    handleClickConfirm() {
+      let temp = this.tempOptions.reduce((pre, cur) => {
+        if (cur.show) {
+          pre.push(this.oriDefaultSlotsObj[cur.prop || cur.type]);
+        }
+        return pre;
+      }, []);
+    //   console.log(this.tempOptions,temp);
+      this.$slots.default = temp;
+      this.options = this.tempOptions;
+      this.show = false;
+    },
+  },
+};
+</script>
---
+++ src/views/customize.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h3>customize page</h3>
+    <base-table :data="tableData">
+      <el-table-column label="姓名" prop="name"></el-table-column>
+      <el-table-column label="年龄" prop="age"></el-table-column>
+      <el-table-column label="性别" prop="gender">
+          <template v-slot="scope">
+              {{scope.row.gender == 1 ? "男":"女"}}
+          </template>
+      </el-table-column>
+      <el-table-column label="邮箱" prop="email"></el-table-column>
+      <el-table-column label="备注" prop="note"></el-table-column>
+    </base-table>
+  </div>
+</template>
+
+<script>
+import BaseTable from "@/components/base-table";
+export default {
+  name: "Customize",
+  components: {
+    BaseTable,
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          name: "汤秀兰",
+          age: 94,
+          gender: 1,
+          email: "i.qofpbiyue@vgvhsk.gi",
+          note: "S",
+        },
+        {
+          name: "黄艳",
+          age: 100,
+          gender: 1,
+          email: "e.wmsafrhcp@dgdwsevo.py",
+          note: "g",
+        },
+        {
+          name: "雷芳",
+          age: 44,
+          gender: 0,
+          email: "x.qslzvgt@tbjwbzmyv.nz",
+          note: "g",
+        },
+        {
+          name: "龙秀兰",
+          age: 119,
+          gender: 0,
+          email: "f.yhtxiqme@gddl.sm",
+          note: "x",
+        },
+        {
+          name: "唐杰",
+          age: 58,
+          gender: 1,
+          email: "g.qjfx@uiye.fr",
+          note: "M",
+        },
+      ],
+    };
+  },
+};
+</script>
