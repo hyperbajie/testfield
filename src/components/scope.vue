cd3243c04a515fc124b60b803c83d21ed94fb363
zhangsen <maizhanwangle@163.com>
2022-03-25 15:23:05 +0800

    feat: add scope experiment

---
+++ src/components/scope.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  props: {
+    message: String,
+  },
+  render(h) {
+    return h("div",{style:"color: pink;"}, [
+      this.$scopedSlots.default(),
+    ]);
+  },
+};
+</script>
---
+++ src/views/ScopeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>ScopeView</h3>
+    <Scope>
+      <div>hello</div>
+    </Scope>
+  </div>
+</template>
+
+<script>
+import Scope from "@/components/scope.vue";
+export default {
+  name: "ScopeView",
+  components: {
+    Scope,
+  },
+};
+</script>
