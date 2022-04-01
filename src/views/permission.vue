73a52225e85780d19dbc7708e926725908313f21
zhangsen <maizhanwangle@163.com>
2022-04-01 16:13:56 +0800

    feat: temporary commit

---
+++ src/views/permission.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h3>permission page</h3>
+    <br />
+    <button v-permission:[tar]="autho">you know</button>
+    <br />
+    <button v-on:click="handleClick">toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "permission",
+  directives: {
+    permission: {
+      inserted: function (el, binding) {
+        const { value, arg } = binding;
+        console.log("value", value, arg)
+        if (arg.indexOf(value) < 0) {
+          el.style.display = "none";
+        } else {
+          el.style.display = "";
+        }
+      },
+      update: function (el, binding) {
+        const { value, arg } = binding;
+        console.log("update");
+        if (arg.indexOf(value) < 0) {
+          el.style.display = "none";
+        } else {
+          el.style.display = "";
+        }
+      },
+    },
+  },
+  data() {
+    return {
+      show: true,
+      autho: "doctor",
+      tar: ["doctor", "dept"],
+    };
+  },
+  methods: {
+    handleClick() {
+      this.show = !this.show;
+      if( this.show ) {
+          this.autho = "doctor";
+      } else {
+          this.autho = "hos";
+      }
+    }
+  },
+};
+</script>
